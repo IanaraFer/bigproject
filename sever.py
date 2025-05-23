33a2597a72c02b550332de6351bc48c526160d8e
IanaraFernandes <ianarafer@gmail.com>
2025-05-23 15:34:58 +0100

    coding

--- sever.py
+++ sever.py
@@ -5,9 +5,9 @@ App = Flask(__name__)
 
 @App.route('/')
 def index():
-        Return "Hello, World!"
-        
-        if __name__ == '__main__':
-            App.run (debug=True)
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    App.run(debug=True)
             
             
